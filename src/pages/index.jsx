affd6c6df1837968ab80da044e2a3b4020265010
zenfumi <ys00878@gmail.com>
2021-05-21 00:07:48 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,25 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-
-const handleClick = (e) => {
-  console.log(e.target.href);
-  e.preventDefault();
-};
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
